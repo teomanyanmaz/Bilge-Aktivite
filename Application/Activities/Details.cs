e07c30c3dfe62a57bdfb72b96c2d1752e8de6997
Teoman Yanmaz <teoman.yanmaz@hotmail.com>
2021-09-17 20:06:06 +0300

    added dto

---
+++ Application/Activities/Details.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Application.Core;
+using Application.Interfaces;
+using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+namespace Application.Activities
+{
+    public class Details
+    {
+        public class Query : IRequest<Result<ActivityDto>>
+        {
+            public Guid Id { get; set; }
+        }
+
+        public class Handler : IRequestHandler<Query, Result<ActivityDto>>
+        {
+            private readonly DataContext _context;
+            private readonly IMapper _mapper;
+            private readonly IUserAccessor _userAccessor;
+            public Handler(DataContext context, IMapper mapper, IUserAccessor userAccessor)
+            {
+                _userAccessor = userAccessor;
+                _mapper = mapper;
+                _context = context;
+            }
+
+            public async Task<Result<ActivityDto>> Handle(Query request, CancellationToken cancellationToken)
+            {
+                var activity = await _context.Activities
+                    .ProjectTo<ActivityDto>(_mapper.ConfigurationProvider, 
+                        new {currentUsername = _userAccessor.GetUsername()})
+                    .FirstOrDefaultAsync(x => x.Id == request.Id);
+
+                return Result<ActivityDto>.Success(activity);
+            }
+        }
+    }
+}
